df9f4a88ab04df17f53908a60bea296e6ff629b4
JerryNguyen23 <62230361+JerryNguyen23@users.noreply.github.com>
2020-04-06 15:44:38 +1000

    Added placeholder target
    Gave target script to destroy on being shot

---
+++ GAM403_A2_JerryNguyen/Assets/Scripts/Target.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Collider col;
+    void Start()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+     Destroy(gameObject);
+    }
+}
